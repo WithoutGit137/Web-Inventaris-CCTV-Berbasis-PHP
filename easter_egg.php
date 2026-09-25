7331c59eca1571a72f108dfdb9ba466b38a40e06
WithoutGit137 <ibrahimhafizh346@gmail.com>
2026-09-25 15:22:27 +0700

    Added Nothing

---
+++ easter_egg.php
@@ -0,0 +1,5 @@
+<html>
+Fairuz Hafizh Ibrahim - 1-7-2026 - 30-9-2026
+<br>Jika seseorang mengakses dan membaca pesan ini, selamat anda telah menemukan sebuah Easter Egg :)
+<br>Have A Nice Day :D
+</html>
